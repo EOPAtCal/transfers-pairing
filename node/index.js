04faf4a9a4e8b406f57cefaa609cde4491693108
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-09-11 00:24:19 -0700

    c

--- node/index.js
+++ node/index.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const readline = require('readline');
-const {
-    google
-} = require('googleapis');
+const { google } = require('googleapis');
 
 // If modifying these scopes, delete credentials.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
@@ -10,9 +8,9 @@ const TOKEN_PATH = 'credentials.json';
 
 // Load client secrets from a local file.
 fs.readFile('client_secret.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content), fetchData);
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), fetchData);
 });
 
 /**
@@ -22,20 +20,19 @@ fs.readFile('client_secret.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-    const {
-        client_secret,
-        client_id,
-        redirect_uris
-    } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        callback(oAuth2Client);
-    });
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
 }
 
 /**
@@ -45,28 +42,28 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', code => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return callback(err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
+        if (err) console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
     });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return callback(err);
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
+  });
 }
 
 /**
@@ -74,81 +71,84 @@ function getNewToken(oAuth2Client, callback) {
  */
 
 function fetchData(auth) {
-    const sheets = google.sheets({
-        version: 'v4',
-        auth
-    });
-    sheets.spreadsheets.values.get({
-        spreadsheetId: menteeDetails.spreadsheetId,
-        range: menteeDetails.range,
-    }, (err, res) => {
-        if (err) return console.log('The API returned an error: ' + err);
-        const rows = res.data.values;
-        if (rows.length) {
-            rows.map((row) => {
-                wrapMentee(row)
-            });
-            writeToFile('mentees.json', JSON.stringify(mentees))
-        } else {
-            console.log('No data found.');
-        }
-    });
+  const sheets = google.sheets({
+    version: 'v4',
+    auth
+  });
+  sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: menteeDetails.spreadsheetId,
+      range: menteeDetails.range
+    },
+    (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const rows = res.data.values;
+      if (rows.length) {
+        rows.map(row => {
+          wrapMentee(row);
+        });
+        writeToFile('mentees.json', JSON.stringify(mentees));
+      } else {
+        console.log('No data found.');
+      }
+    }
+  );
 
-    sheets.spreadsheets.values.get({
-        spreadsheetId: mentorDetails.spreadsheetId,
-        range: mentorDetails.range,
-    }, (err, res) => {
-        if (err) return console.log('The API returned an error: ' + err);
-        const rows = res.data.values;
-        if (rows.length) {
-            rows.map((row) => {
-                wrapMentor(row)
-            });
-            writeToFile('mentors.json', JSON.stringify(mentors))
-        } else {
-            console.log('No data found.');
-        }
-    });
+  sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: mentorDetails.spreadsheetId,
+      range: mentorDetails.range
+    },
+    (err, res) => {
+      if (err) return console.log('The API returned an error: ' + err);
+      const rows = res.data.values;
+      if (rows.length) {
+        rows.map(row => {
+          wrapMentor(row);
+        });
+        writeToFile('mentors.json', JSON.stringify(mentors));
+      } else {
+        console.log('No data found.');
+      }
+    }
+  );
 }
 
-const mentees = []
-const mentors = []
-
+const mentees = [];
+const mentors = [];
 
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, 'utf8', function (err, _) {
-        if (err) {
-            console.error(`unable to write ${fileName}`);
-        } else {
-            console.log(`wrote ${fileName}`)
-        }
-    })
+  fs.writeFile(fileName, data, 'utf8', function(err, _) {
+    if (err) {
+      console.error(`unable to write ${fileName}`);
+    } else {
+      console.log(`wrote ${fileName}`);
+    }
+  });
 }
 
-const wrapMentee = mentee => mentees.push({
-    name: `${mentee[2]} ${mentee[3]} ${mentee[4]}`,
-    email: mentee[9],
-    college: mentee[11],
-    major: mentee[13],
-    id: mentee[9]
-});
-
-// const processString = (str) => str.toLowerCase().trim()
+const wrapMentee = mentee =>
+  mentees.push({
+    id: mentee[8],
+    email: mentee[8],
+    college: mentee[9],
+    major: mentee[28]
+  });
 
 const wrapMentor = mentor =>
-    mentors.push({
-        name: mentor[1],
-        email: mentor[2],
-        major: mentor[4],
-        college: mentor[5],
-        id: mentor[2]
-    })
+  mentors.push({
+    id: mentor[3],
+    email: mentor[3],
+    college: mentor[4],
+    major: mentor[19],
+    limit: mentor[20]
+  });
 
 const menteeDetails = {
-    spreadsheetId: "1hYJI9U5R4e1-bOhSdNowPPk-cskxrIX4p3Cqtk29II0",
-    range: 'A2:AI376',
-}
+  spreadsheetId: '1hRQ5v0pao0zh_LSH3zY9LgsSooDyaVjTaxO7cMOR56g',
+  range: 'A2:AC351'
+};
 const mentorDetails = {
-    spreadsheetId: "18zmzgEI6DzS-r_GRr9SuE5OSk6fIE2RWuJb7AJv8AgE",
-    range: "A2:I47"
-}
+  spreadsheetId: '1BRs3GAmGsz7ehTc7l6fkzmB7klSvDqcpvZGYBlGYnMQ',
+  range: 'A2:U61'
+};
--- node/rules.js
+++ node/rules.js
@@ -6,16 +6,16 @@ function match(mentors, mentees) {
   let menteeIdx = 0;
   let matchReason;
 
-  setup(mentors)
+  setup(mentors);
 
   while (mentors.length > 0) {
-    mentee = mentees[menteeIdx]
+    mentee = mentees[menteeIdx];
     if ((mentorIdx = firstMajorAndCollege(mentee, mentors)) > -1) {
-      matchReason = "college & major"
+      matchReason = 'college & major';
     } else if ((mentorIdx = firstCollegeMatch(mentee, mentors)) > -1) {
-      matchReason = "college"
+      matchReason = 'college';
     } else if ((mentorIdx = firstMajorMatch(mentee, mentors)) > -1) {
-      matchReason = "major"
+      matchReason = 'major';
     } else {
       menteeIdx++;
       if (menteeIdx > mentees.length - 1) {
@@ -23,12 +23,12 @@ function match(mentors, mentees) {
       }
       continue;
     }
-    mentor = mentors[mentorIdx]
-    pair(mentor.id, mentee, matchReason)
+    mentor = mentors[mentorIdx];
+    pair(mentor.id, mentee, matchReason);
     if (isMentorFullyPaired(mentor.id)) {
-      mentors = filterIdx(mentors, mentorIdx)
+      mentors = filterIdx(mentors, mentorIdx);
     }
-    menteeIdx++
+    menteeIdx++;
     if (menteeIdx > mentees.length - 1) {
       break;
     }
@@ -36,7 +36,7 @@ function match(mentors, mentees) {
 }
 
 function pair(mentorId, mentee, matchReason) {
-  pairResults[mentorId].mentees.push(mentee)
+  pairResults[mentorId].mentees.push(mentee);
   pairResults[mentorId].matchReasons.push(matchReason);
 }
 
@@ -45,66 +45,71 @@ function showPairsOnly(pairResults) {
   for (const mentorId in pairResults) {
     results.push({
       mentor: mentorId,
-      mentees: prettyPrintLists(pairResults[mentorId].mentees, "id"),
+      mentees: prettyPrintLists(pairResults[mentorId].mentees, 'id'),
       matchReason: prettyPrintLists(pairResults[mentorId].matchReasons)
-    })
+    });
   }
   return results;
 }
 
-const prettyPrintLists = (lst, attr) => lst.reduce((acc, item, idx) =>
-  acc + `${attr ? item[attr] : item}${idx === lst.length - 1 ? '' : ', '}`, "");
-
+const prettyPrintLists = (lst, attr) =>
+  lst.reduce(
+    (acc, item, idx) =>
+      acc + `${attr ? item[attr] : item}${idx === lst.length - 1 ? '' : ', '}`,
+    ''
+  );
 
 function removeUnpaired(pairResults) {
   const results = Object.assign(pairResults);
   for (const mentorId in results) {
     if (results[mentorId].mentees.length === 0) {
-      unmatched.push(mentorId)
-      delete results[mentorId]
+      unmatched.push(mentorId);
+      delete results[mentorId];
     }
   }
   return results;
 }
 
-const isMentorFullyPaired = (mentorId) => {
+const isMentorFullyPaired = mentorId => {
   const mentor = pairResults[mentorId];
   return mentor.maxMenteesSize === mentor.mentees.length;
-}
+};
 
-const filterIdx = (lst, index) => lst.filter((_, idx) => idx !== index)
+const filterIdx = (lst, index) => lst.filter((_, idx) => idx !== index);
 
-const firstMajorMatch = (mentee, mentors) => mentors.findIndex(mentor => mentor.major === mentee.major)
+const firstMajorMatch = (mentee, mentors) =>
+  mentors.findIndex(mentor => mentor.major === mentee.major);
 
-const firstCollegeMatch = (mentee, mentors) => mentors.findIndex(mentor => mentor.college === mentee.college)
+const firstCollegeMatch = (mentee, mentors) =>
+  mentors.findIndex(mentor => mentor.college === mentee.college);
 
-const firstMajorAndCollege = (mentee, mentors) => mentors.findIndex(mentor => mentor.college === mentee.college && mentor.major === mentee.major)
+const firstMajorAndCollege = (mentee, mentors) =>
+  mentors.findIndex(
+    mentor => mentor.college === mentee.college && mentor.major === mentee.major
+  );
 
-
-let pairResults = {}
+let pairResults = {};
 let unmatched = [];
 
 function setup(mentors) {
-  mentors.forEach((mentor) => {
+  mentors.forEach(mentor => {
     pairResults[mentor.id] = {
       mentees: [],
       maxMenteesSize: 4,
       matchReasons: []
-    }
-  })
+    };
+  });
 }
 
 function main() {
-  const mentees = require('./mentees.json')
-  const mentors = require('./mentors.json')
-  console.log(`${mentees.length} mentees and ${mentors.length} mentors`)
-
-  match(mentors, mentees)
-  pairResults = removeUnpaired(pairResults)
-  pairResults = showPairsOnly(pairResults)
-
-  console.log(pairResults)
-  console.log(unmatched.length)
+  const mentees = require('./mentees.json');
+  const mentors = require('./mentors.json');
+  console.log(`${mentees.length} mentees and ${mentors.length} mentors`);
+  match(mentors, mentees);
+  pairResults = removeUnpaired(pairResults);
+  pairResults = showPairsOnly(pairResults);
+  console.log(pairResults);
+  console.log(unmatched.length);
 }
 
-main()
+main();
